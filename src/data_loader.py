6dd64ee1e31cb87c53a1c23ab5a7f5c2b8dfe670
Reza Ghanbari <ghanbari.reza94@gmail.com>
2023-02-18 10:00:59 +0100

    Fine tunning the model.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,76 +1,64 @@
-import glob
-import os
-
+import os, glob
 import tensorflow as tf
+from jax.lib import xla_bridge
+
+PATH = 'datasets/'
+BATCH_SIZE = 12
+IMAGE_SIZE = 512
+DEVICE = xla_bridge.get_backend().platform
+
+def read_train_data():
+    x_files = tf.data.Dataset.list_files(PATH + "train/*.png")
+    y_files = tf.data.Dataset.list_files(PATH + "mask/*.png")
+
+    def read_image(x_filename, y_filename):
+        x_image_string = tf.io.read_file(x_filename)
+        y_image_string = tf.io.read_file(y_filename)
+
+        x_image_decoded = tf.image.decode_png(x_image_string, channels=3)
+        y_image_decoded = tf.image.decode_png(y_image_string, channels=1)
+
+        x_image_resized = tf.image.resize(x_image_decoded, [IMAGE_SIZE, IMAGE_SIZE])
+        y_image_resized = tf.image.resize(y_image_decoded, [IMAGE_SIZE, IMAGE_SIZE])
+
+        x_image_norm = x_image_resized / 255
+        y_image_norm = y_image_resized / 255
+
+        return x_image_norm, y_image_norm
+
+    dataset = tf.data.Dataset.zip((x_files, y_files))
+
+    dataset = dataset.map(read_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.shuffle(1000).batch(BATCH_SIZE)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    return dataset
 
 
-class DataLoader:
-    
-    def __init__(self, path='../datasets/', batch_size=12, image_size=512):
-        self.path = path
-        self.batch_size = batch_size
-        self.image_size = image_size
+def read_predict_data():
+    def image_generator():
+        for filename in glob.iglob(PATH + "test/*.png"):
+            with open(filename, "rb") as f:
+                image = tf.image.decode_png(f.read(), channels=3)
+                image_resized = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
+                image_norm = image_resized / 255.0
+                yield image_norm
 
-    
-    def read_train_data(self):
-        x_files = tf.data.Dataset.list_files(self.path + "train/*.png", shuffle=True)
-        y_files = tf.data.Dataset.list_files(self.path + "mask/*.png", shuffle=True)
-
-        def read_image(x_filename, y_filename):
-            x_image_string = tf.io.read_file(x_filename)
-            y_image_string = tf.io.read_file(y_filename)
-
-            x_image_decoded = tf.image.decode_png(x_image_string, channels=3) # RGB image
-            y_image_decoded = tf.image.decode_png(y_image_string, channels=1) # grayscale image
-
-            x_image_resized = tf.image.resize(x_image_decoded, [self.image_size, self.image_size])
-            y_image_resized = tf.image.resize(y_image_decoded, [self.image_size, self.image_size])
-
-            x_image_norm = x_image_resized / 255
-            y_image_norm = y_image_resized / 255
-
-            return x_image_norm, y_image_norm
-
-        
-        dataset = tf.data.Dataset.zip((x_files, y_files))
-
-        dataset = dataset.interleave(
-            lambda x: tf.data.Dataset.from_tensor_slices(x),
-            cycle_length=tf.data.AUTOTUNE,
-            num_parallel_calls=tf.data.AUTOTUNE
-        )
-
-        dataset = dataset.map(read_image, num_parallel_calls=tf.data.AUTOTUNE)
-        dataset = dataset.shuffle(1000).batch(self.batch_size)
-        dataset = dataset.prefetch(tf.data.AUTOTUNE)
-
-        return dataset
-
-    def read_predict_data(self):
-        file_path = '../datasets/test/pre.png'
-        image = tf.io.read_file(file_path)
-        image_decoded = tf.image.decode_png(image, channels=3)
-        image_resized = tf.image.resize(image_decoded, [self.image_size, self.image_size])
-        image_norm = image_resized / 255
-
-        return image_norm
-
-    def save_image(self, image):
-        file_path = '../datasets/test/result.png'
-        image = image * 255
-
-        encode_image = tf.image.encode_jpeg(image, format='rgb', quality=100)
-
-        with open(file_path, 'wb') as fd:
-            fd.write(encode_image)
+    images_dataset = tf.data.Dataset.from_generator(
+        image_generator,
+        output_types=tf.float32,
+        output_shapes=(IMAGE_SIZE, IMAGE_SIZE, 3),
+    )
+    images_dataset = images_dataset.batch(BATCH_SIZE)
+    images_dataset = images_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    return images_dataset
 
 
-# processor = ImageProcessor()
+def save_image(image):
+    file_path = 'predict/result.jpg'
+    image = image * 255
 
-# train_data = processor.read_train_data()
-# predict_data = processor.read_predict_data()
+    encode_image = tf.image.encode_jpeg(image, format='rgb', quality=100)
 
-# # Perform training and prediction using the data
-# # ...
-
-# processor.save_image(result_image)
+    with open(file_path, 'wb') as fd:
+        fd.write(encode_image)
--- src/model.py
+++ src/model.py
@@ -4,50 +4,210 @@ from flax import linen as fnn
 
 
 class Encoder(fnn.Module):
+    """
+    This code defines a class called Encoder, 
+    which is a component of a UNet architecture
+    commonly used in image segmentation tasks. 
+    The Encoder class takes as input an image 
+    tensor and produces a set of skip connections, 
+    which are used by the Decoder network to 
+    generate a segmentation mask.
+
+    The Encoder network consists of a series of 
+    convolutional layers with increasing numbers 
+    of channels and decreasing spatial dimensions.
+    Each set of features is passed through batch 
+    normalization and activation functions. The 
+    network also includes max-pooling and dropout 
+    layers in the higher-resolution feature maps 
+    to reduce overfitting and improve generalization.
+
+    The Encoder class has two class variables: 
+    features and training. features is an integer 
+    that specifies the number of channels in the 
+    convolutional layers of the network. training 
+    is a boolean that specifies whether the network
+    is in training mode or not.
+
+    The __call__ method of the Encoder class is defined
+    using the @fnn.compact decorator, which is used in
+    the JAX library to define neural networks in a 
+    concise manner. The __call__ method takes the input
+    image tensor x as input and returns a list of skip 
+    connections as output.
+
+    The Encoder network applies a series of convolutional 
+    layers to the input image, producing a set of feature 
+    maps with different spatial dimensions and channel sizes. 
+    The network then applies batch normalization and ReLU 
+    activation functions to each set of features. Max-pooling
+    is used in the lower-resolution feature maps to reduce 
+    the spatial dimensions and provide translation invariance.
+    Dropout is used in the higher-resolution feature maps to 
+    reduce overfitting and improve generalization.
+
+    The output of the Encoder network is a list of feature maps 
+    at multiple scales. The feature maps in the list are in 
+    decreasing order of spatial dimensions and increasing order 
+    of channel sizes. The list of feature maps is used as input 
+    to the Decoder network, which upsamples and combines the 
+    features to produce a segmentation mask.
+
+    Overall, the Encoder class defines the feature extraction 
+    and downsampling operations of the UNet architecture, 
+    which are crucial for producing accurate segmentation 
+    masks in image segmentation tasks.
+
+    """
     features: int = 64
     training: bool = True
 
     @fnn.compact
     def __call__(self, x):
-        z = x
         skips = []
-        for i in range(4):
-            z = fnn.Conv(self.features * 2 ** i, kernel_size=(3, 3))(z)
+        for i in range(5):
+            z = fnn.Conv(self.features * 2 ** i, kernel_size=(3, 3))(x)
             z = fnn.relu(z)
             z = fnn.Conv(self.features * 2 ** i, kernel_size=(3, 3))(z)
             z = fnn.BatchNorm(use_running_average=not self.training)(z)
             z = fnn.relu(z)
-            if i > 2:
+            if i < 4:
+                x = fnn.max_pool(z, window_shape=(2, 2), strides=(2, 2))
+            if i == 3 or i == 4:
                 z = fnn.Dropout(0.5, deterministic=False)(z)
-            if i < 3:
-                z = fnn.max_pool(z, window_shape=(2, 2), strides=(2, 2))
             skips.append(z)
-
+            
         return skips
 
 
 class Decoder(fnn.Module):
+    """ This code defines a class called Decoder, 
+        which is a component of a UNet architecture 
+        commonly used in image segmentation tasks. 
+        The Decoder class takes as input a set of 
+        skip connections z_skips, which are produced 
+        by the Encoder network and contain feature 
+        maps at multiple scales.
+        
+        The Decoder class has two class variables: 
+        features and training. features is an integer 
+        that specifies the number of channels in the 
+        convolutional layers of the network. 
+        training is a boolean that specifies whether 
+        the network is in training mode or not.
+        
+        The __call__ method of the Decoder class is
+        defined using the @fnn.compact decorator, which 
+        is used in the JAX library to define neural networks
+        in a concise manner. The __call__ method takes 
+        the set of skip connections z_skips as input and 
+        returns a segmentation mask as output.
+
+        The Decoder network first selects the last set of
+        features from z_skips, and then iteratively upsamples
+        and concatenates the feature maps from the other sets 
+        of skip connections with the current set of features. 
+        The concatenated features are then passed through a 
+        series of convolutional layers and activation functions 
+        to produce the final segmentation mask.
+
+        The output of the Decoder network is a single-channel 
+        tensor with the same spatial dimensions as the input 
+        images. Each pixel in the output tensor represents the
+        probability that the corresponding pixel in the input 
+        image belongs to the object or class being segmented. 
+        The sigmoid activation function is used to ensure that 
+        the output values are in the range [0, 1].
+
+        Overall, the Decoder class defines the upsampling and 
+        feature combination operations of the UNet architecture, 
+        which are crucial for producing accurate segmentation masks
+        in image segmentation tasks.
+
+    """
     features: int = 64
     training: bool = True
 
     @fnn.compact
-    def __call__(self, skips):
-        z = skips[-1]
-        for i in range(3):
-            z = jax.image.resize(z, shape=(z.shape[0], z.shape[1] * 2, z.shape[2] * 2, z.shape[3]), method='nearest')
-            z = jnp.concatenate([skips[-i-2], z], axis=3)
-            z = fnn.Conv(self.features * 2 ** (3 - i), kernel_size=(3, 3))(z)
+    def __call__(self, z_skips):
+        z = z_skips[-1]
+        for i in range(4):
+            z = jax.image.resize(z, 
+                                 shape=(z.shape[0], z.shape[1] * 2, z.shape[2] * 2, z.shape[3]), 
+                                 method='nearest')
+            z = fnn.Conv(self.features * 8, kernel_size=(2, 2))(z)
             z = fnn.relu(z)
-            z = fnn.Conv(self.features * 2 ** (3 - i), kernel_size=(3, 3))(z)
+            z = jnp.concatenate([z_skips[-i-2], z], axis=3)
+            z = fnn.Conv(self.features * 8, kernel_size=(3, 3))(z)
+            z = fnn.relu(z)
+            z = fnn.Conv(self.features * 8, kernel_size=(3, 3))(z)
             z = fnn.BatchNorm(use_running_average=not self.training)(z)
             z = fnn.relu(z)
 
         z = fnn.Conv(1, kernel_size=(1, 1))(z)
         z = fnn.sigmoid(z)
+
         return z
 
 
 class UNet(fnn.Module):
+    """This code defines a class called UNet, 
+       which is a type of convolutional neural 
+       network commonly used for image segmentation 
+       tasks. The UNet architecture consists of 
+       an encoder and a decoder network, which are 
+       connected through skip connections.
+       The class has two class variables: features 
+       and training. features is an integer that 
+       specifies the number of channels in the 
+       convolutional layers of the network. training 
+       is a boolean that specifies whether the 
+       network is in training mode or not.
+
+    Args:
+        fnn (__call__): 
+        The __call__ method of the UNet class is 
+        defined using the @fnn.compact decorator, 
+        which is used in the JAX library to define 
+        neural networks in a concise manner.
+        The __call__ method takes an input tensor x, 
+        which is passed through the Encoder network 
+        to produce a set of skip connections. 
+        These skip connections are then passed through 
+        the Decoder network to produce the final
+        output tensor y.
+        The Encoder network is responsible for 
+        downsampling the input tensor and extracting 
+        high-level features, while the Decoder network 
+        is responsible for upsampling the features 
+        and producing the final segmentation mask.
+
+    Returns:
+        _type_: 
+        The return value of the __call__ method in 
+        this code is a tensor y, which represents 
+        the output of the UNet architecture after 
+        processing the input tensor x. The exact
+        shape and data type of y will depend on the 
+        specific implementation of the Decoder 
+        network and the nature of the image segmentation
+        task that the UNet is being used for.
+        In general, the output tensor y will likely 
+        be a multi-channel tensor with the same spatial 
+        dimensions as the input tensor x. Each channel 
+        of y may represent a different segmentation class 
+        or feature of the input image, depending on the 
+        task at hand. The data type of y will likely 
+        be a floating-point type, such as float32, 
+        to allow for the computation of gradients 
+        during backpropagation.
+    
+    Overall, this code defines a UNet architecture 
+    that can be used for image segmentation tasks. 
+    By changing the value of the features and 
+    training variables, the architecture can be 
+    customized to fit different input data and training regimes.
+    """
     features: int = 64
     training: bool = True
 
@@ -57,5 +217,3 @@ class UNet(fnn.Module):
         y = Decoder(self.training)(z_skips)
 
         return y
-
-
